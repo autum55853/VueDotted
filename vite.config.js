71274f7b7b1ed2013599256c0621a8e81020b105
Amber Lin <410103037@gms.ndhu.edu.tw>
2023-03-04 18:05:26 +0800

    Updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./VueDotted",
+  base: "./",
   plugins: [vue()],
   resolve: {
     alias: {
